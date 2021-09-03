f4474b8fde6587d678586096c9479be50af95595
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-09-03 18:58:31 -0400

    Move business object from BusinessList to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,17 +8,32 @@ import React from 'react';
 import './App.css';
 
 //Import Components
-import BusinessList from '../BusinessList/BusinessList';
-import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BusinessList.js';
+import SearchBar from '../SearchBar/SearchBar.js';
 
+//Business object will be a stand in for the data pulled from the yelp api at a later date
 
-export default class App extends React.Component {
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businessArray = [business, business, business, business, business, business];
+
+export class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>gourmand</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList business={businessArray} />
       </div>
     );
   }
