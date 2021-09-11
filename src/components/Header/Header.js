b7b00866d84d06b7a8f22e4d34883332fc1e4f00
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-09-11 18:46:02 -0400

    Create Header.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+//Import React
+import React from 'react';
+
+//Import Style
+import './Header.css';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <section className="header">
+                <h1>gourmand</h1>
+                <h2>.</h2>
+            </section>
+
+        )
+    }
+}
+
+export default Header;
