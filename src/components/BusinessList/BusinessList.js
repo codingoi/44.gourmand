17a560430ead6cf445d43a9af433772ceb484e9b
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-09-11 18:49:20 -0400

    Update BusinessList.js

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -8,14 +8,26 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
+
     render() {
         return (
-            <div className='BusinessList'>
-                <div className="BusinessList">
-                    {this.props.business.map((business) => {
-                        return <Business business={business}/>
-                    })}
-                </div>
+            <div className="BusinessList">
+                {
+                    typeof this.props.business !== 'string' ?
+                        <>{this.props.term ?
+                            <h2>Results for {this.props.term.toUpperCase()}</h2>
+                            : <></>
+                        }</>
+                        : <div><h3>{this.props.business}</h3><p>Each search must have: <strong>Where ? (location)</strong></p></div>
+                }
+                
+                    {
+                        typeof this.props.business !== 'string' ? <div style={{margin: '0 0 0 9.375%'}}>{this.props.business.map((business) => {
+                            return <Business business={business} key={business.id} />
+                        })}</div> : <></>
+                    }
+
+               
             </div>
         );
     }
