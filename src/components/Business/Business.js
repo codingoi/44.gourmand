8c2b1f8b07cd029e34e1402d13dbd21578855321
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-09-11 18:50:18 -0400

    Update Business.js

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -11,20 +11,42 @@ class Business extends React.Component {
         return (
             <div className="Business">
                 <div className="image-container">
-                    <img src={this.props.business.imageSrc} alt={this.props.business.name} />
+                    <img src={this.props.business.image_url} alt={this.props.business.name} />
+                </div>
+
+                <div className="overlay overlay-left">
+                <div className="Business-reviews">
+                            
+                            <h3 className="rating">{this.props.business.rating}
+                                <sub className="material-icons">
+                                    star
+                                </sub>
+                            </h3>
+                        </div>
+                    <div className="Business-information">
+                        <div className="Business-address">
+                            {this.props.business.location.display_address.map((detail, i) => {
+                                return <p key={i}>{detail}</p>
+                            })}
+
+                            <p>{this.props.business.zipCode}</p>
+                            <p>{this.props.business.display_phone}</p>
+                        </div>
+
+                        
+                    </div>
+                    
                 </div>
                 <h2>{this.props.business.name}</h2>
-                <div className="Business-information">
-                    <div className="Business-address">
-                        <p>{this.props.business.address}</p>
-                        <p>{this.props.business.city}</p>
-                        <p>{this.props.business.zipCode}</p>
-                    </div>
-                    <div className="Business-reviews">
-                        <h3>{this.props.business.category}</h3>
-                        <h3 className="rating">{this.props.business.rating}</h3>
-                        <p>{this.props.business.reviewCount}</p>
-                    </div>
+                <div className="Business-categories">
+                    {this.props.business.categories[0].title}
+                    <sup>.</sup>
+                    <span>
+                    {this.props.business.rating}
+                    <sub className="material-icons">
+                                    star
+                                </sub>
+                    </span>
                 </div>
             </div>
         );
