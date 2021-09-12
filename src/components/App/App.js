bb203b07eb556bc6b133a23ca200bc00a5c2eb06
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-09-11 20:01:20 -0400

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,42 +6,75 @@ import './App.css';
 
 //Import Components
 import BusinessList from '../BusinessList/BusinessList.js';
+import Header from '../Header/Header';
 import SearchBar from '../SearchBar/SearchBar.js';
+import Recommendations from '../Recommendations/Recommendations';
+import Gallery from '../Gallery/Gallery'
 
-//Business object will be a stand in for the data pulled from the yelp api at a later date
+//Import Yelp API Call function which will return a business object
+import Yelp from '../../util/Yelp';
 
-const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
-
-const businessArray = [
-  business, 
-  business, 
-  business, 
-  business, 
-  business, 
-  business
-];
+//Import Geolocation function which will locate the device and feed information to the TopOption component
+import Geolocation from '../../util/Geolocation';
 
 export class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: [],
+      recommendations: [],
+      location: {},
+      term: '',
+    }
+
+    this.getLocation = this.getLocation.bind(this);
+    this.searchYelp = this.searchYelp.bind(this);
+    this.yelpRecommendations = this.yelpRecommendations.bind(this);
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+
+
+  async getLocation() {
+    if ('geolocation' in navigator) {
+      const response = await navigator.geolocation.getCurrentPosition((position) => {
+        this.setState({ location: position.coords })
+      })
+    } else {
+      this.setState({ location: { latitude: 40.7666688, longitude: -73.990144 } })
+    }
+  }
+
+  yelpRecommendations(term, location, sortBy) {
+    Yelp.search(term, location, sortBy)
+      .then((businesses) => {
+        this.setState({ recommendations: businesses })
+      })
+  }
+
+
+
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    Yelp.search(term, location, sortBy)
+      .then((businesses) => {
+        this.setState({ businesses: businesses, term: `${term} in ${location}` || location })
+      })
   }
 
   render() {
     return (
       <div className="App">
-        <h1>gourmand</h1>
-        <SearchBar searchYelp={this.searchYelp}/>
-        <BusinessList business={businessArray} />
+        <Header />
+        {
+          (this.state.location.longitude === undefined) ? <p></p> : <><Recommendations location={this.state.location} findTopRated={this.yelpRecommendations} />
+            <Gallery business={this.state.recommendations} /></>
+        }
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList business={this.state.businesses} term={this.state.term} />
       </div>
     );
   }
