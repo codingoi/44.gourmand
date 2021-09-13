168f1b8d30f487f98d9fd90b14218fd80317daff
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-09-13 01:17:33 -0400

    Update Yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -8,14 +8,16 @@ const Yelp = {
         //Temporary CORS Anywhere URL - CORS Anywhere will take requests sent to its API endpoint, make them for the requesting app with the proper CORS permissions, and then return the response back to the requesting app.
         const cors = 'https://cors-anywhere.herokuapp.com/'
 
-        //Yelp Endpoint
+        //URL is the variable that will be used to when calling the API and represents the request endpoint
         let url;
     
-        //Combined cors and Yelp endpoint
+        //Combined cors and Yelp defined link and parameters
         if(typeof location === 'object'){
+            //Used to build Popular Restarant recommendation list based on geolocation api results
             const endpointLatLong = `https://api.yelp.com/v3/businesses/search?term=${term}&latitude=`+location.latitude+'&longitude='+location.longitude+`&sort_by=${sortBy}&limit=10&radius=8050`;
              url = cors + endpointLatLong;
         } else {
+            //Used by build the businesslist componenet that is based of the user search query
             const endpoint = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
             url = cors + endpoint;
         }
@@ -40,7 +42,6 @@ const Yelp = {
             })
             .then((jsonResponse) => {
                 if (jsonResponse.businesses) {
-                    console.log("busy", jsonResponse.businesses)
                     return jsonResponse.businesses.map((business) => {
                         const { id, image_url, name, location, display_phone, categories, rating, reviewCount, transactions } = business;
                         return {
